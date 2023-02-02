33444711e6760032b58cd517c39e1774b02239e8
OpenTech-Consult <opentech.consult@gmail.com>
2023-02-02 04:50:15 +0000

    Hobnob
    Continuing initial setup
    
    Changed the content of .babelrc specifying the current version of node.js

--- compiled.js
+++ compiled.js
@@ -1,10 +1,13 @@
-import "@babel/polyfill";
-import http from 'http';
+"use strict";
+
+require("@babel/polyfill");
+var _http = _interopRequireDefault(require("http"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 const requestHandler = function (req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/plain'
   });
   res.end('Hello, world');
 };
-const server = http.createServer(requestHandler);
+const server = _http.default.createServer(requestHandler);
 server.listen(8080);
