7d883c51674b8caa0ab8a36af8ecb9763d823f11
OpenTech-Consult <opentech.consult@gmail.com>
2023-02-02 06:02:19 +0000

    Hobnob
    Initial Setup: automating development using nodemon
    
    installed the nodemon package
    added the "watch" script to instruct the nodemon to watch for change the in scr directory and to rebuild its contents

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ const requestHandler = function (req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/plain'
   });
-  res.end('Hello, world');
+  res.end('Hello, !! Coming at you slowly');
 };
 const server = _http.default.createServer(requestHandler);
 server.listen(8080);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import http from 'http'
 
 const requestHandler = function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Hello, world')
+    res.end('Hello, !! Coming at you slowly')
 }
 
 const server = http.createServer(requestHandler)
